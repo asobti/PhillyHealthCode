dfd67b5f98882ddb838452aadae46bc8c7e856a9
Mayank <mayank.gureja@gmail.com>
2013-05-28 17:28:50 -0400

    Moved to IntelliJ

--- VitaminME/src/com/vitaminme/android/BaseActivity.java
+++ VitaminME/src/com/vitaminme/android/BaseActivity.java
@@ -4,139 +4,134 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.app.SlidingFragmentActivity;
-import com.vitaminme.android.R;
 
 public class BaseActivity extends SlidingFragmentActivity
 {
-	public String helpMessage = "";
+    public String helpMessage = "";
 
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
-		// Set sidebar view
-		setBehindContentView(R.layout.menu_frame);
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.menu_frame, new SidebarFragment()).commit();
+        // Set sidebar view
+        setBehindContentView(R.layout.menu_frame);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.menu_frame, new SidebarFragment()).commit();
 
-		// Customize Sidebar
-		SlidingMenu sm = getSlidingMenu();
-		sm.setShadowWidthRes(R.dimen.shadow_width);
-		sm.setShadowDrawable(R.drawable.shadow);
-		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		sm.setFadeEnabled(true);
-		sm.setFadeDegree(0.35f);
-		sm.setBehindScrollScale(0.25f);
-		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
-		sm.setSlidingEnabled(true);
-		setSlidingActionBarEnabled(false);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Customize Sidebar
+        SlidingMenu sm = getSlidingMenu();
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setShadowDrawable(R.drawable.shadow);
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setFadeEnabled(true);
+        sm.setFadeDegree(0.35f);
+        sm.setBehindScrollScale(0.25f);
+        sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        sm.setSlidingEnabled(true);
+        setSlidingActionBarEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-	}
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Set ActionBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Set ActionBar
 
-		final AlertDialog.Builder helpDialog = new AlertDialog.Builder(this);
-		menu.add("Help").setIcon(R.drawable.info)
-				.setOnMenuItemClickListener(new OnMenuItemClickListener()
-				{
-					@Override
-					public boolean onMenuItemClick(MenuItem item)
-					{
-						helpDialog
-								.setTitle("Help")
-								.setMessage(helpMessage)
-								.setIcon(R.drawable.info)
-								.setPositiveButton("OK",
-										new DialogInterface.OnClickListener()
-										{
-											public void onClick(
-													DialogInterface dialog,
-													int id)
-											{
+        final AlertDialog.Builder helpDialog = new AlertDialog.Builder(this);
+        menu.add("Help").setIcon(R.drawable.info)
+                .setOnMenuItemClickListener(new OnMenuItemClickListener()
+                {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item)
+                    {
+                        helpDialog
+                                .setTitle("Help")
+                                .setMessage(helpMessage)
+                                .setIcon(R.drawable.info)
+                                .setPositiveButton("OK",
+                                        new DialogInterface.OnClickListener()
+                                        {
+                                            public void onClick(
+                                                    DialogInterface dialog,
+                                                    int id)
+                                            {
 
-											}
-										}).show();
-						return false;
-					}
-				}).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+                                            }
+                                        }).show();
+                        return false;
+                    }
+                }).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
 
-		final AlertDialog.Builder aboutDialog = new AlertDialog.Builder(this);
-		menu.add("About Yummly").setIcon(R.drawable.info)
-				.setOnMenuItemClickListener(new OnMenuItemClickListener()
-				{
-					@Override
-					public boolean onMenuItemClick(MenuItem item)
-					{
-						View view = getLayoutInflater().inflate(
-								R.layout.about_yummly, null);
+        final AlertDialog.Builder aboutDialog = new AlertDialog.Builder(this);
+        menu.add("About Yummly").setIcon(R.drawable.info)
+                .setOnMenuItemClickListener(new OnMenuItemClickListener()
+                {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item)
+                    {
+                        View view = getLayoutInflater().inflate(
+                                R.layout.about_yummly, null);
 
-						Typeface tf = Typeface.createFromAsset(getAssets(),
-								"fonts/Lato-Bold.ttf");
-						Typeface tf2 = Typeface.createFromAsset(getAssets(),
-								"fonts/Lato-Regular.ttf");
+                        Typeface tf = Typeface.createFromAsset(getAssets(),
+                                "fonts/Lato-Bold.ttf");
+                        Typeface tf2 = Typeface.createFromAsset(getAssets(),
+                                "fonts/Lato-Regular.ttf");
 
-						TextView tv1 = (TextView) view
-								.findViewById(R.id.aboutVitaminText);
-						tv1.setTypeface(tf);
-						TextView tv2 = (TextView) view
-								.findViewById(R.id.aboutYummlyText);
-						tv2.setTypeface(tf2);
-						TextView tv3 = (TextView) view
-								.findViewById(R.id.aboutYummlyLink);
-						tv3.setTypeface(tf2);
+                        TextView tv1 = (TextView) view
+                                .findViewById(R.id.aboutVitaminText);
+                        tv1.setTypeface(tf);
+                        TextView tv2 = (TextView) view
+                                .findViewById(R.id.aboutYummlyText);
+                        tv2.setTypeface(tf2);
+                        TextView tv3 = (TextView) view
+                                .findViewById(R.id.aboutYummlyLink);
+                        tv3.setTypeface(tf2);
 
-						aboutDialog
-								.setView(view)
-								.setIcon(R.drawable.info)
-								.setPositiveButton("OK",
-										new DialogInterface.OnClickListener()
-										{
-											public void onClick(
-													DialogInterface dialog,
-													int id)
-											{
+                        aboutDialog
+                                .setView(view)
+                                .setIcon(R.drawable.info)
+                                .setPositiveButton("OK",
+                                        new DialogInterface.OnClickListener()
+                                        {
+                                            public void onClick(
+                                                    DialogInterface dialog,
+                                                    int id)
+                                            {
 
-											}
-										}).show();
-						return false;
-					}
-				}).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+                                            }
+                                        }).show();
+                        return false;
+                    }
+                }).setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
 
-		return true;
-	}
+        return true;
+    }
 
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-		case android.R.id.home:
-			toggle();
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                toggle();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public void onBackPressed()
-	{
-		super.onBackPressed();
-		finish();
-	}
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        finish();
+    }
 
 }
